344154667ae23d634097f4d5e2fe26119696dfe8
karinaaduu@gmail.com <karinaaduu@gmail.com>
2023-04-04 18:49:03 +0300

    Touch Control for Android added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,25 +6,33 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D Controller;
     public Animator Animator;
+    public Joystick Joystick;
 
     public float RunSpeed = 40f;
     float horizontalMove = 0f;
     bool jump=false;
 
-    // Start is called before the first frame update
-
-    // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * RunSpeed;
+        //horizontalMove = Input.GetAxisRaw("Horizontal") * RunSpeed;
 
+        horizontalMove = Joystick.Horizontal * RunSpeed;
+
+        float verticalMove = Joystick.Vertical;
         Animator.SetFloat("Speed",Mathf.Abs(horizontalMove));
 
-        if (Input.GetButtonDown("Jump"))
+        if (verticalMove >= .5f)
         {
             jump = true;
-            Animator.SetBool("IsJumping",true);
+            Animator.SetBool("IsJumping", true);
         }
+
+
+        //if (Input.GetButtonDown("Jump"))
+        //{
+        //    jump = true;
+        //    Animator.SetBool("IsJumping",true);
+        //}
     }
 
     public void OnLanding()
